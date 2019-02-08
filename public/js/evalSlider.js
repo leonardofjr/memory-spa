69a8dca5cb97d69a482946fcb59cd4392b51622a
Leo <leonardo.f.jr@gmail.com>
2019-02-08 15:05:11 -0500

    /

--- public/js/evalSlider.js
+++ public/js/evalSlider.js
@@ -1,4 +1,5 @@
-    function evalSlider(id) {
-        let sliderValue = $('#' + id).val();
-         $('#' + id ).next().html(sliderValue);
-    }
+/* This slider is used to update the Skills Range Input, it'll display the output value */
+function evalSlider(id) {
+    let sliderValue = $('#' + id).val();
+        $('#' + id ).next().html(sliderValue);
+}
